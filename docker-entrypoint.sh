441ffe11f6f60530ede095bb8aef9d914458ef6d
Jonathan Stoppani <jonathan@stoppani.name>
2016-04-22 12:15:02 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = 'redis-server' ]; then
     chown -R redis .
-    envsubst < /etc/redis.conf | exec gosu redis "$@"
+    envsubst < /etc/redis.conf | exec su-exec redis "$@"
 fi
 
 exec $@
